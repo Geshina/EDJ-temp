869f3257d4051c452274b1fde93a125d124f728d
Geshina <geshovskileo@gmail.com>
2024-06-10 13:39:55 +0200

    hardcoded sale

--- src/main.js
+++ src/main.js
@@ -1,47 +1,47 @@
-import './osseus/CSS/_config.css'
-import './osseus/CSS/_custom.css'
-import './osseus/CSS/color.css'
-import './osseus/CSS/reset.css'
-import './osseus/CSS/structure.css'
-import './osseus/CSS/typography.css'
-import './osseus/CSS/utility.css'
+import './oss-temp/CSS/_config.css'
+import './oss-temp/CSS/_custom.css'
+import './oss-temp/CSS/color.css'
+import './oss-temp/CSS/reset.css'
+import './oss-temp/CSS/structure.css'
+import './oss-temp/CSS/typography.css'
+import './oss-temp/CSS/utility.css'
 
-import { globalEventHandler } from './osseus/JS/handlers'
-import { singleStateToggle } from './osseus/JS/state'
+import { globalEventHandler } from './oss-temp/JS/handlers'
+import { singleStateToggle } from './oss-temp/JS/state'
 import { Cart, Product } from './shop'
 
 let two_day_signature = new Product(
-  'Two_Day_Signature',
-  'Дводневен signature',
-  2000
+	'Two_Day_Signature',
+	'Дводневен signature',
+	2000
 )
 let three_day_signature = new Product(
-  'Three_Day_Signature',
-  'Тридневен signature',
-  2850
+	'Three_Day_Signature',
+	'Тридневен signature',
+	2850
 )
 let four_day_signature = new Product(
-  'Four_Day_Signature',
-  'Четиридневен signature',
-  3600
+	'Four_Day_Signature',
+	'Четиридневен signature',
+	3600
 )
 ///
 let two_day_immuno = new Product('Two_Day_Immuno', 'Дводневен immuno', 1900)
 let three_day_immuno = new Product('Three_Day_Immuno', 'Тридневен immuno', 2700)
 let four_day_immuno = new Product(
-  'Four_Day_Immuno',
-  'Четиридневен immuno',
-  3400
+	'Four_Day_Immuno',
+	'Четиридневен immuno',
+	3400
 )
 
 let cart = new Cart(
-  two_day_signature,
-  three_day_signature,
-  four_day_signature,
-  //
-  two_day_immuno,
-  three_day_immuno,
-  four_day_immuno
+	two_day_signature,
+	three_day_signature,
+	four_day_signature,
+	//
+	two_day_immuno,
+	three_day_immuno,
+	four_day_immuno
 )
 
 globalEventHandler('click', '._faq_origin', singleStateToggle)
--- src/shop.js
+++ src/shop.js
@@ -1,60 +1,60 @@
 import { preparePayment } from './firebase'
 
 class Product {
-  constructor(name, cyrName, price, amount = 0) {
-    this.name = name
-    this.cyrName = cyrName
-    this.price = price
-    this.amount = amount
-  }
-  get total() {
-    return this.amount * this.price
-  }
+	constructor(name, cyrName, price, amount = 0) {
+		this.name = name
+		this.cyrName = cyrName
+		this.price = price
+		this.amount = amount
+	}
+	get total() {
+		return this.amount * this.price
+	}
 }
 
 class Cart {
-  constructor(...products) {
-    this.products = this.sessionStorageGet() || products
-    this.salePercent = 0
-  }
-  addAmount = (dataObj) => {
-    const product = this.products.find((p) => p.name === dataObj.origin.name)
-    if (product) product.amount = Math.min(6, ++product.amount)
-    this.sessionStorageSet()
-    this.updateDOM()
-  }
-  removeAmount = (dataObj) => {
-    const product = this.products.find((p) => p.name === dataObj.origin.name)
-    if (product) product.amount = Math.max(0, --product.amount)
-    this.sessionStorageSet()
-    this.updateDOM()
-  }
-  get total() {
-    return this.products.reduce(
-      (total, product) => total + product.total * (1 - this.salePercent / 100),
-      0
-    )
-  }
+	constructor(...products) {
+		this.products = this.sessionStorageGet() || products
+		this.salePercent = 15
+	}
+	addAmount = (dataObj) => {
+		const product = this.products.find((p) => p.name === dataObj.origin.name)
+		if (product) product.amount = Math.min(6, ++product.amount)
+		this.sessionStorageSet()
+		this.updateDOM()
+	}
+	removeAmount = (dataObj) => {
+		const product = this.products.find((p) => p.name === dataObj.origin.name)
+		if (product) product.amount = Math.max(0, --product.amount)
+		this.sessionStorageSet()
+		this.updateDOM()
+	}
+	get total() {
+		return this.products.reduce(
+			(total, product) => total + product.total * (1 - this.salePercent / 100),
+			0
+		)
+	}
 
-  updateDOM = () => {
-    const updates = [
-      ...this.products.map(({ name, amount }) => [`.${name}_amount`, amount]),
-      ['.cart-total', this.total],
-      // Add more mappings here
-    ]
-    updates.forEach(([selector, value]) => {
-      const element = document.querySelector(selector)
-      if (element) element.textContent = value
-      // if (!element) console.warn(`${selector} not found`)
-    })
+	updateDOM = () => {
+		const updates = [
+			...this.products.map(({ name, amount }) => [`.${name}_amount`, amount]),
+			['.cart-total', this.total],
+			// Add more mappings here
+		]
+		updates.forEach(([selector, value]) => {
+			const element = document.querySelector(selector)
+			if (element) element.textContent = value
+			// if (!element) console.warn(`${selector} not found`)
+		})
 
-    const cont = document.querySelector('.product-cont')
-    if (!cont) return console.warn(`${cont} not found`)
+		const cont = document.querySelector('.product-cont')
+		if (!cont) return console.warn(`${cont} not found`)
 
-    cont.innerHTML = this.products
-      .filter((product) => product.amount > 0)
-      .map(
-        ({ name, cyrName, amount }) => `
+		cont.innerHTML = this.products
+			.filter((product) => product.amount > 0)
+			.map(
+				({ name, cyrName, amount }) => `
         <div class="flex-column justify-between">
           <div class="flex-row justify-between">
               <p>${cyrName}</p>
@@ -66,122 +66,122 @@ class Cart {
           </div>
         </div>
       `
-      )
-      .join('')
-  }
-  sessionStorageSet = () => {
-    sessionStorage.setItem('cart', JSON.stringify(this.products))
-  }
-  sessionStorageGet = () => {
-    return JSON.parse(sessionStorage.getItem('cart'))?.map(
-      ({ name, cyrName, price, amount }) =>
-        new Product(name, cyrName, price, amount) || null
-    )
-  }
+			)
+			.join('')
+	}
+	sessionStorageSet = () => {
+		sessionStorage.setItem('cart', JSON.stringify(this.products))
+	}
+	sessionStorageGet = () => {
+		return JSON.parse(sessionStorage.getItem('cart'))?.map(
+			({ name, cyrName, price, amount }) =>
+				new Product(name, cyrName, price, amount) || null
+		)
+	}
 
-  checkForm = (dataObj) => {
-    const errorMessages = {
-      full_name: 'Внесете име и презиме',
-      Address: 'Внесете ја вашата адреса',
-      Email: 'Внесете ја вашата е-мејл адреса',
-      Phone: 'Внесете го вашиот телефонски број',
-    }
-    const form = document.querySelector(dataObj.target.selector)
-    const formArr = Array.from(form.elements).map((input) => ({
-      name: input.name,
-      value: input.value.trim(),
-      errorMsg: errorMessages[input.name] || null,
-      target: document.querySelector(`label[for="${input.name}"]`),
-    }))
+	checkForm = (dataObj) => {
+		const errorMessages = {
+			full_name: 'Внесете име и презиме',
+			Address: 'Внесете ја вашата адреса',
+			Email: 'Внесете ја вашата е-мејл адреса',
+			Phone: 'Внесете го вашиот телефонски број',
+		}
+		const form = document.querySelector(dataObj.target.selector)
+		const formArr = Array.from(form.elements).map((input) => ({
+			name: input.name,
+			value: input.value.trim(),
+			errorMsg: errorMessages[input.name] || null,
+			target: document.querySelector(`label[for="${input.name}"]`),
+		}))
 
-    let hasError = false
+		let hasError = false
 
-    formArr.forEach((inputObj) => {
-      if (inputObj.value === '' && inputObj.errorMsg) {
-        inputObj.target.textContent = inputObj.errorMsg
-        inputObj.target.classList.add('error-red')
-        hasError = true
-      }
-    })
+		formArr.forEach((inputObj) => {
+			if (inputObj.value === '' && inputObj.errorMsg) {
+				inputObj.target.textContent = inputObj.errorMsg
+				inputObj.target.classList.add('error-red')
+				hasError = true
+			}
+		})
 
-    return !hasError
-  }
-  mapForm = () => {
-    return Array.from(document.querySelector('#deliveryInfo').elements).reduce(
-      (acc, input) => {
-        if (input.name) {
-          acc[input.name] = input.value.trim()
-        }
-        return acc
-      },
-      {}
-    )
-  }
+		return !hasError
+	}
+	mapForm = () => {
+		return Array.from(document.querySelector('#deliveryInfo').elements).reduce(
+			(acc, input) => {
+				if (input.name) {
+					acc[input.name] = input.value.trim()
+				}
+				return acc
+			},
+			{}
+		)
+	}
 
-  proxyRequest = async (dataObj) => {
-    //    Checks
-    let cartStatus = () => {
-      return !this.products.every((obj) => obj.amount === 0)
-    }
-    if (cartStatus() === false) {
-      document.querySelector('._global_error').textContent =
-        'Немате Продукти во Кошничка'
-      return
-    }
-    let formStatus = this.checkForm(dataObj)
-    if (formStatus == false) {
-      document.querySelector('._global_error').textContent =
-        'Потребно е сите полиња да се пополнети'
-      return
-    }
-    // let productStatus = this.products.some((product) => product.amount === 0)
-    // if (!productStatus) {
-    //   document.querySelector('._global_error').textContent =
-    //     'Немате продукти во кошничка'
-    // }
+	proxyRequest = async (dataObj) => {
+		//    Checks
+		let cartStatus = () => {
+			return !this.products.every((obj) => obj.amount === 0)
+		}
+		if (cartStatus() === false) {
+			document.querySelector('._global_error').textContent =
+				'Немате Продукти во Кошничка'
+			return
+		}
+		let formStatus = this.checkForm(dataObj)
+		if (formStatus == false) {
+			document.querySelector('._global_error').textContent =
+				'Потребно е сите полиња да се пополнети'
+			return
+		}
+		// let productStatus = this.products.some((product) => product.amount === 0)
+		// if (!productStatus) {
+		//   document.querySelector('._global_error').textContent =
+		//     'Немате продукти во кошничка'
+		// }
 
-    // products
-    let response = null
-    // console.log(this.mapForm())
-    // console.log(this.products)
-    try {
-      response = await preparePayment({
-        products: this.products,
-        form: this.mapForm(),
-      })
-      console.log(response)
-    } catch (error) {
-      console.log(error)
-    }
+		// products
+		let response = null
+		// console.log(this.mapForm())
+		// console.log(this.products)
+		try {
+			response = await preparePayment({
+				products: this.products,
+				form: this.mapForm(),
+			})
+			console.log(response)
+		} catch (error) {
+			console.log(error)
+		}
 
-    const orderedKeys = [
-      'AmountToPay',
-      'PayToMerchant',
-      'MerchantName',
-      'AmountCurrency',
-      'Details1',
-      'Details2',
-      'PaymentFailURL',
-      'PaymentOKURL',
-      'CheckSumHeader',
-      'CheckSum',
-      'Address',
-      'Telephone',
-      'Email',
-    ]
+		const orderedKeys = [
+			'AmountToPay',
+			'PayToMerchant',
+			'MerchantName',
+			'AmountCurrency',
+			'Details1',
+			'Details2',
+			'PaymentFailURL',
+			'PaymentOKURL',
+			'CheckSumHeader',
+			'CheckSum',
+			'Address',
+			'Telephone',
+			'Email',
+		]
 
-    let formHtml =
-      '<form action="https://www.cpay.com.mk/client/Page/default.aspx?xml_id=/mk-MK/.loginToPay/.simple/" method="post" id="paymentForm">'
-    orderedKeys.forEach((key) => {
-      if (!response.format[key]) console.warn(`Form data doesnt contain ${key}`)
-      formHtml += `<input type="hidden" name="${key}" value="${response.format[key]}" />`
-    })
+		let formHtml =
+			'<form action="https://www.cpay.com.mk/client/Page/default.aspx?xml_id=/mk-MK/.loginToPay/.simple/" method="post" id="paymentForm">'
+		orderedKeys.forEach((key) => {
+			if (!response.format[key]) console.warn(`Form data doesnt contain ${key}`)
+			formHtml += `<input type="hidden" name="${key}" value="${response.format[key]}" />`
+		})
 
-    formHtml += '<button type="submit">Pay</button></form>'
-    document.body.insertAdjacentHTML('beforeend', formHtml)
-    console.log(formHtml)
-    document.getElementById('paymentForm').submit()
-  }
+		formHtml += '<button type="submit">Pay</button></form>'
+		document.body.insertAdjacentHTML('beforeend', formHtml)
+		console.log(formHtml)
+		document.getElementById('paymentForm').submit()
+	}
 }
 
 export { Product, Cart }
